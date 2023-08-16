ca43a12ab1fbbe1ca1305824169e95c021e48e71
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-17 00:35:50 +0200

    239

--- easy/0001. Two Sum.py
+++ easy/0001. Two Sum.py
@@ -3,6 +3,7 @@
 
 from typing import List
 
+
 class Solution(object):
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         nums_map = {}
@@ -10,7 +11,6 @@ class Solution(object):
             nums_map[nums[i]] = i
         for i, num in enumerate(nums):
             if target - num in nums_map and i != nums_map[target - num]:
-                return [i, nums_map[target - num]]  
+                return [i, nums_map[target - num]]
 
 # print(Solution().twoSum([2,7,11,15], 9))
-
---
+++ hard/0239. Sliding Window Maximum.py
@@ -0,0 +1,43 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(n)
+
+from collections import deque
+from typing import List
+
+
+class Solution(object):
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        que = deque()
+        result = []
+
+        # def que_values():
+        #     return list(map(lambda x: nums[x], que))
+
+        def pop_smaller_than_new_elem_and_append_it():
+            while que and nums[i] >= nums[que[-1]]:
+                que.pop()
+            que.append(i)
+
+        def pop_out_of_window():
+            while que and que[0] <= i-k:
+                que.popleft()
+
+        def append_result():
+            result.append(nums[que[0]])
+
+        for i in range(k):
+            pop_smaller_than_new_elem_and_append_it()
+
+        for i in range(k, len(nums)):
+            append_result()
+
+            pop_out_of_window()
+            pop_smaller_than_new_elem_and_append_it()
+
+        append_result()
+
+        return result
+
+
+# expected [3,3,5,5,6,7]
+# print(Solution().maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], k=3))
