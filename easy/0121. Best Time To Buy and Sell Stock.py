7e01b0dd9446ff84ff7c61148e1dae2c5fe53112
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-17 02:30:28 +0200

    0020 and 0121

--- easy/0121. Best Time To Buy and Sell Stock.py
+++ easy/0121. Best Time To Buy and Sell Stock.py
@@ -8,14 +8,12 @@ from math import inf
 class Solution(object):
     def maxProfit(self, prices: List[int]) -> int:
         best = 0
-        sell = -inf
         buy = inf
         for p in prices:
-            sell = max(sell, p)
             if p < buy:
-                sell = buy = p
-            best = max(best, sell - buy)
+                buy = p
+            best = max(best, p - buy)
         return best
 
 
-# print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))  # expected 5
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))  # expected 5
