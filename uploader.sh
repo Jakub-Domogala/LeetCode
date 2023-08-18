0a9d61f36d08e4fe41c16eb7418423af5b1704f4
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-19 01:37:26 +0200

    1

--- uploader.sh
+++ uploader.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [ "$1" == "upload" ] && [ "$2" ]; then
-    # Run generate.py
-    python readme_generator.py
 
-    # Git pull to get the latest changes
-    git pull origin main
+# Run generate.py
+python readme_generator.py
 
-    # Add all changes to the staging area
-    git add .
+# Git pull to get the latest changes
+git pull origin main
 
-    # Commit with the provided argument as the commit message
-    git commit -m "$2"
+# Add all changes to the staging area
+git add .
 
-    # Push the changes to the remote repository
-    git push origin main
+# Commit with the provided argument as the commit message
+git commit -m "$1"
 
-    echo "Upload completed successfully!"
-else
-    echo "Usage: $0 upload 'commit message'"
-fi
+# Push the changes to the remote repository
+git push origin main
+
+echo "Upload completed successfully!"
