7a87ad7495029f9ca6dbf364c82b5319b22c636b
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-19 02:03:35 +0200

    test

--- uploader.sh
+++ uploader.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # prepared only to be run from repo root dir
-python 4.utilities/readme_generator.py
+git checkout master
 git pull
 git add .
 git commit -m "$1"
+git push
+python 4.utilities/readme_generator.py
+git checkout master
+git pull
+git add .
+git commit -m "Readme Updated"
 git push
