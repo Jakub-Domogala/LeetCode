258f028f41bf6f878428a004532ea982692f3c82
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-20 19:31:59 +0200

    0012

---
+++ 2.medium/0012. Integer to Roman.py
@@ -0,0 +1,24 @@
+# Time Complexity:   O(c)
+# Memory Complexity: O(c)
+
+class Solution(object):
+    def intToRoman(self, num):
+        int2rom = {
+            1   : 'I',
+            4   : 'IV', 5    : 'V',
+            9   : 'IX', 10   : 'X',
+            40  : 'XL', 50   : 'L',
+            90  : 'XC', 100  : 'C',
+            400 : 'CD', 500  : 'D',
+            900 : 'CM', 1000 : 'M'
+        }
+
+        result = ""
+        for key in sorted(int2rom.keys(), reverse=True):
+            amount = num//key
+            result += int2rom[key] * amount
+            num %= key
+        return result
+
+
+# print(Solution().intToRoman(927))
