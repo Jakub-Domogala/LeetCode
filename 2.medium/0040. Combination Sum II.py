01888cdb62d7f032baeab9cb2dace6331f8feb77
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-25 01:22:36 +0200

    0040

---
+++ 2.medium/0040. Combination Sum II.py
@@ -0,0 +1,22 @@
+# Time Complexity:   O(target)
+# Memory Complexity: O(target)
+# NOTE: Negative numbers are not considered in this solution
+
+from typing import List
+
+
+class Solution(object):
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def rec(candidates, sum = 0):
+            if sum == target:
+                return [[]]
+            if len(candidates) == 0 or sum + candidates[0] > target:
+                return []
+            result = []
+            result += [[candidates[0]] + result for result in rec(candidates[1:], sum + candidates[0])]
+            result += rec([elem for elem in candidates if elem != candidates[0]], sum)
+            return result
+        return rec(sorted(candidates))
+    
+# print(Solution().combinationSum([10,1,2,7,6,1,5], 8))
+            
