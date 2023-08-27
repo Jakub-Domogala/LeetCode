00776cd6285dcd3e11fcaa86044a7bd2d0115844
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-27 23:58:47 +0200

    0070

--- 1.easy/0070. Climbing Stairs.py
+++ 1.easy/0070. Climbing Stairs.py
@@ -1,15 +1,27 @@
 # Time Complexity:   O(n) with cache
 # Memory Complexity: O(n) with cache
 
-from functools import cache
+# Time Complexity:   O(2^n) without cache
+# Memory Complexity: O(1) without cache
 
 
 class Solution(object):
-    @cache
+    # not cached version
+    # def climbStairs(self, n: int) -> int:
+    #     if n <= 1:
+    #         return 1
+    #     return self.climbStairs(n-1) + self.climbStairs(n-2)
+
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        memorize = [None] * (n + 1)
+        def fib_memorized(to_go = n):
+            if memorize[to_go]:
+                return memorize[to_go]
+            if to_go <= 1:
+                return 1
+            memorize[to_go] = fib_memorized(to_go-1) + fib_memorized(to_go-2)
+            return memorize[to_go]
+        return fib_memorized()
+        
 
-
-print(Solution().climbStairs(5))
+print(Solution().climbStairs(50))
