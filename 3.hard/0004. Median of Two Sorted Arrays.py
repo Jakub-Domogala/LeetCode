e54dd010be8d79298302cd71e6948c867072ca30
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-27 22:03:26 +0200

    0004

--- 3.hard/0004. Median of Two Sorted Arrays.py
+++ 3.hard/0004. Median of Two Sorted Arrays.py
@@ -1,48 +1,46 @@
+# Time Complexity:   O(log(n))
+# Memory Complexity: O(1)
+# Where n is summary length of arrays
+from typing import List
+
+
 class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        def get_values(arr: List[int], idx: int):
+            p_val  = arr[idx] if idx >= 0 else float("-infinity")
+            next_val = arr[idx + 1]  if idx + 1 < len(arr) else float("infinity")
+            return p_val, next_val
+        
         A, B = nums1, nums2
         n = len(nums1) + len(nums2)
-        half = n // 2
+        half_n = n // 2
+
         if len(A) > len(B):
             A, B = B, A
-        
+
         l, r = 0, len(A) - 1
+
         while True:
             i = (l + r) // 2
-            j = half - i - 2
+            j = half_n - i - 2
 
-            A_left  = A[i]      if i >= 0           else float("-infinity")
-            B_left  = B[j]      if j >= 0           else float("-infinity")
-            A_right = A[i + 1]  if i + 1 < len(A)   else float("infinity")
-            B_right = B[j + 1]  if j + 1 < len(B)   else float("infinity")
+            A_pivot_val, A_next_val = get_values(A, i)
+            B_pivot_val, B_next_val = get_values(B, j)
 
-            if A_left <= B_right and B_left <= A_right:
-                print(A_left, A_right, B_left, B_right)
+            if A_pivot_val <= B_next_val and B_pivot_val <= A_next_val:
                 if n % 2:
-                    return min(A_right, B_right)
-                return float(max(A_left, B_left) + min(A_right, B_right)) / 2
-            elif A_left > B_right:
+                    return min(A_next_val, B_next_val)
+                return float(max(A_pivot_val, B_pivot_val) + min(A_next_val, B_next_val)) / 2
+            elif A_pivot_val > B_next_val:
                 r = i - 1
             else:
                 l = i + 1
 
-
-        
-
-            
-
-
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: float
-        """
-
-# res 5
-
-# 1,2,2,3,4,5,6,7,8,9,9,10
+# res 5.5
 # a = [2,4,5,6,7,8,9]
 # b = [1,2,3,9,10]
-a = [1,2]
-b = [3,4]
-print(Solution().findMedianSortedArrays(a, b))
+
+# res 3
+# a = [1,2]
+# b = [3,4,5]
+# print(Solution().findMedianSortedArrays(a, b))
