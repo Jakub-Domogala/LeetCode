341d7971b70467a3fb9b36275977d6495245b4f3
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-27 22:53:12 +0200

    0058

---
+++ 1.easy/0058. Length of Last Word.py
@@ -0,0 +1,16 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(1)
+
+class Solution(object):
+    def lengthOfLastWord(self, s: str) -> int:
+        # return len(s.split()[-1])
+        n = len(s)
+        index = n - 1
+        while index >= 0 and s[index] == " ":
+            index -= 1
+        for i in range(index, -1, -1):
+            if s[i] == " ":
+                return index - i
+        return index + 1
+
+# print(Solution().lengthOfLastWord("   fly me   to   the moon  "))
