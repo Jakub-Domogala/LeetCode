6c5179108db69b80f2a503aaf3f10313fdc85ea9
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-27 23:29:09 +0200

    0069

--- 1.easy/0069. Sqrt(x).py
+++ 1.easy/0069. Sqrt(x).py
@@ -1,6 +1,19 @@
+# Time Complexity:   O(log(n))
+# Memory Complexity: O(1)
+
 class Solution(object):
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+        return right
+
+# print(Solution().mySqrt(6)) # 2
+# print(Solution().mySqrt(9)) # 3
+# print(Solution().mySqrt(10)) # 3
