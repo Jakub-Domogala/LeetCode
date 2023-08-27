14aef5dd065347d87b6b83cbad9cd34029484d4f
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-27 23:13:11 +0200

    0066

---
+++ 1.easy/0066. Plus One.py
@@ -0,0 +1,19 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(1)
+
+from typing import List
+
+
+class Solution(object):
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] += 1
+
+        index = len(digits) - 1
+        while index > 0 and digits[index] > 9:
+            digits[index-1], digits[index] = digits[index-1] + digits[index]//10, digits[index]%10
+            index -= 1
+        return digits if digits[0] < 10 else [digits[0]//10] + [digits[0]%10] + digits[1:]
+
+
+# arr = [9]
+# print(Solution().plusOne(arr))
