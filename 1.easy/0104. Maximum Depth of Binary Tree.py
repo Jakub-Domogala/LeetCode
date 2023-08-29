b1465bd349040555802ff6643d38131a33490f2d
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-29 23:24:29 +0200

    0104

---
+++ 1.easy/0104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,19 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(n)
+# Where n is amount of elements in the Tree
+
+from utilities.TreeNode import TreeNode
+
+
+class Solution(object):
+    def maxDepth(self, root: TreeNode) -> int:
+        def get_depth(node: TreeNode) -> int:
+            if node is None:
+                return 0
+            return max(get_depth(node.left), get_depth(node.right)) + 1
+        return get_depth(root)
+
+
+# q = [3,9,20,None,None,15,7]
+# q = TreeNode().arr2tree(q)
+# print(Solution().maxDepth(q))
