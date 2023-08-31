4a6302b307383670e34dfb8d4f812d37eb6f9075
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-31 21:22:29 +0200

    0095

--- 2.medium/0095. Unique Binary Search Trees II.py
+++ 2.medium/0095. Unique Binary Search Trees II.py
@@ -1,31 +1,31 @@
+# Time Complexity:   Hard to calculate
+# Memory Complexity: Hard to calculate
+
+from typing import List
 from utilities.TreeNode import TreeNode
 
 
 class Solution(object):
-    def generateTrees(self, n):
-        def insert_elem(root, elem):
-            if root is None:
-                return TreeNode(elem)
-            if root.val < elem:
-                root.right = insert_elem(root.right, elem)
-                return root
-            else:
-                root.left = insert_elem(root.left, elem)
-                return root
-        root = None
-        # for i in range(n):
-        #     root = insert_elem(root, i)
-        # return root
-        def get_trees_from(nums = nums):
+    def generateTrees(self, n: int) -> List[TreeNode]:
+        sub_trees_memo = {}
+        def generate(left_num: int, right_num: int) -> List[TreeNode]:
+            if left_num > right_num:
+                return [None]
+            if (left_num, right_num) in sub_trees_memo:
+                sub_trees_memo[(left_num, right_num)]
             result = []
-            if len(nums) == 0:
-                return []
-            for i in nums:
-                
-            
-        # result = []
-        # nums = [i for i in range(1, n+1)]
-        # for i in nums:
-        #     result += 
-
-print(Solution().generateTrees(3))
+            for val in range(left_num, right_num + 1):
+                for left_tree in generate(left_num, val - 1):
+                    for right_tree in generate(val + 1, right_num):
+                        root = TreeNode(val, left_tree, right_tree)
+                        result.append(root)  
+            sub_trees_memo[(left_num, right_num)] = result
+            return result
+        return generate(1,n)
+    
+              
+# sol = Solution().generateTrees(3)
+# print(len(sol), 'trees in total')
+# for tree in sol:
+#     print(tree)
+#     print('='*70)
