fbecbecc2a4ceefdc76fc3cdaaf239792d598d46
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-08-31 22:06:37 +0200

    0098

---
+++ 2.medium/0098. Validate Binary Search Tree.py
@@ -0,0 +1,23 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(n)
+
+from utilities.TreeNode import TreeNode
+
+
+class Solution(object):
+    def isValidBST(self, root):
+        def isValid(root, min_val = float('-infinity'), max_val = float('infinity')):
+            if not root:
+                return True
+            return (min_val < root.val < max_val          and 
+                    isValid(root.left, min_val, root.val) and 
+                    isValid(root.right, root.val, max_val))
+        return isValid(root)
+        
+# tree1 = TreeNode().arr2tree([5,1,4,None,None,3,6])
+# tree2 = TreeNode().arr2tree([3,1,4])
+# print(Solution().isValidBST(tree1))
+# print(tree1)
+# print('='*70)
+# print(Solution().isValidBST(tree2))
+# print(tree2)
