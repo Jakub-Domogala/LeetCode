e0465ac00dbab747959f621dcf6aa0707d7cd9cc
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-10-19 00:01:03 +0200

    0704

---
+++ 1.easy/0704. Binary Search.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return -1
+
+
+print(Solution().search([1, 2, 3, 6, 7], 2))
