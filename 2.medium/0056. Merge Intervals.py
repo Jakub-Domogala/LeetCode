9197e44d6583f3a5e6c7a622afd3e168773feb2f
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2023-10-23 14:37:33 +0200

    0056

--- 2.medium/0056. Merge Intervals.py
+++ 2.medium/0056. Merge Intervals.py
@@ -7,20 +7,22 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda x: x[1])
-        print(intervals)
+        intervals = sorted(intervals, key=lambda x: x[0])
         result = []
         curr_i = intervals[0]
-        for i in intervals[1:]:
+        for i in intervals:
             if curr_i[1] < i[0]:
                 result.append(curr_i)
                 curr_i = i
             else:
                 curr_i[1] = max(curr_i[1], i[1])
-        result.append(curr_i)
-        print(result)
+        return result + [curr_i]
 
 
 # intervals = [[1, 4], [4, 5]]
+# print(intervals)
+# print(Solution().merge(intervals))
+# print()
 # intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+# print(intervals)
 # print(Solution().merge(intervals))
