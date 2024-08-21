3ace8281a0ba0067bfeab56c6a05af74f3c2e310
Jakub-Domogala <kuba.domogala@gmail.com>
2024-08-21 18:12:44 +0200

    0136

--- 1.easy/0136. Single Number.py
+++ 1.easy/0136. Single Number.py
@@ -2,7 +2,8 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        print(2 * sum(set(nums)) - sum(nums))
+        return 2 * sum(set(nums)) - sum(nums)
 
 nums = [2,2,1]
-Solution().singleNumber(nums)
+result = Solution().singleNumber(nums)
+print(result)  # 1
