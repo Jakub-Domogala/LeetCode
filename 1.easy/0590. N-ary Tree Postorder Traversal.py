053120f33d4bd440120f229692f53de2d72cbee6
Jakub-Domogala <kuba.domogala@gmail.com>
2024-10-03 14:39:14 +0200

    0590

--- 1.easy/0590. N-ary Tree Postorder Traversal.py
+++ 1.easy/0590. N-ary Tree Postorder Traversal.py
@@ -4,14 +4,14 @@
 # To better understand this solution head to similiar problem 0144 which includes visualisations
 
 class Solution:
-    def preorder(self, root: 'Node') -> List[int]:
+    def postorder(self, root: 'Node') -> List[int]:
         result = []
         def dive(node):
             nonlocal result
             if node is None:
                 return
-            result += [node.val]
             for c in node.children:
                 dive(c)
+            result += [node.val]
         dive(root)
         return result
