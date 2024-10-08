f629054e6d93f7fd5727c857cfabadfe2891eefc
Jakub-Domogala <kuba.domogala@gmail.com>
2024-10-09 00:09:57 +0200

    generator

--- utilities/readme_generator.py
+++ utilities/readme_generator.py
@@ -1,6 +1,7 @@
 import os
 import urllib.parse
 from FileCounter import FileCounter
+from math import log10, floor
 
 repo_owner = "Jakub-Domogala"
 repo_name = "LeetCode"
@@ -67,8 +68,9 @@ with open(output_file, "w") as f:
     f.write(
         "### " + str(count) + " solutions in total"
     )
-    f.write("\n`" + "{:<4d}`".format(dir_count_dict['./1.easy']) + " easy problems\n")
-    f.write("\n`" + "{:<4d}`".format(dir_count_dict['./2.medium']) + " medium problems\n")
-    f.write("\n`" + "{:<4d}`".format(dir_count_dict['./3.hard']) + " hard problems\n")
+    counters_size = floor(log10(max(dir_count_dict.values()))) + 1
+    f.write("\n`" + "{:<{counters_size}d}`".format(dir_count_dict['./1.easy']) + " easy problems\n")
+    f.write("\n`" + "{:<{counters_size}d}`".format(dir_count_dict['./2.medium']) + " medium problems\n")
+    f.write("\n`" + "{:<{counters_size}d}`".format(dir_count_dict['./3.hard']) + " hard problems\n")
     f.write("\n--- \n")
     f.write(get_file_content(input_after))
