a2fbf484b52eece25bf756cfe398f391dd1b6fc9
Jakub-Domogala <kuba.domogala@gmail.com>
2024-10-14 04:19:28 +0200

    add various bin searches and KMP search algorithm

---
+++ algorithms/KMPSearch.py
@@ -0,0 +1,47 @@
+# Time Complexity:   O(n+m)
+# Memory Complexity: O(n+result)
+
+def KMPSearch(pat, txt):
+    def computeLPSArray(pat):
+        M = len(pat)
+        lps = [0] * M
+        length = 0
+        i = 1
+        while i < M:
+            if pat[i] == pat[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+
+    M = len(pat)
+    N = len(txt)
+    lps = computeLPSArray(pat)
+    result = []
+    i = 0  # index for txt
+    j = 0  # index for pat
+    while (N - i) >= (M - j):
+        if pat[j] == txt[i]:
+            j += 1
+            i += 1
+        if j == M:
+            result.append(i - j)
+            j = lps[j - 1]
+        elif i < N and pat[j] != txt[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+    return result
+
+
+txt = "aaaa"
+pat = "aaa"
+result = KMPSearch(pat, txt)
+print(result)
--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -13,5 +13,53 @@ def binary_search(arr: List[int], target: int):
             return mid
     return -1
 
+def binary_search_geq(arr: List[int], target: int):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return left if left < len(arr) else -1
 
-# print(binary_search([1,2,3,4], 2))
+def binary_search_leq(arr: List[int], target: int):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            return mid
+    return right
+
+def binary_search_for_range(arr: List[int], target_min: int, target_max: int):
+    i_min = binary_search_geq(arr, target_min)
+    i_max = binary_search_leq(arr, target_max)
+    if i_min != -1 and i_max != -1 and i_min <= i_max:
+        return i_min, i_max
+    return None
+
+print(binary_search([1,2,3,4], 2), 1)
+print(binary_search([1,3,4], -1), -1)
+print()
+print(binary_search_geq([1,3,4,5,7,8], 2), 1)
+print(binary_search_geq([1,3,4,5,7,8], 3), 1)
+print(binary_search_geq([1,3,4,5,7,8], 10), -1)
+print(binary_search_geq([1,3,4,5,7,8], -10), 0)
+print()
+print(binary_search_leq([1,3,4,5,7,8], 2), 0)
+print(binary_search_leq([1,3,4,5,7,8], 3), 1)
+print(binary_search_leq([1,3,4,5,7,8], 10), 5)
+print(binary_search_leq([1,3,4,5,7,8], -10), -1)
+print()
+print(binary_search_for_range([1,3,4,5,7,8], 2, 5), [1,3])
+print(binary_search_for_range([1,3,4,5,7,8], 3, 5), [1,3])
+print(binary_search_for_range([1,3,4,5,7,8], 10, 12), None)
+print(binary_search_for_range([1,3,4,5,7,8], -10, 10), [0,5])
+print(binary_search_for_range([1,3,4,5,7,8], 5, 5), [3,3])
+print(binary_search_for_range([1,3,4,5,7,8], 6, 6), None)
