b7eef575825b71874ac49b858d218d77f83912be
Jakub-Domogala <kuba.domogala@gmail.com>
2024-10-24 03:15:25 +0200

    0009

--- 1.easy/0009. Palindrome Number.py
+++ 1.easy/0009. Palindrome Number.py
@@ -2,7 +2,22 @@
 # Memory Complexity: O(log_10(n))
 
 
+from math import log10, floor
+
 class Solution(object):
+    # def isPalindrome(self, x: int) -> bool: # this is just a challenge to not use string
+    #     if x < 0:
+    #         return False
+    #     if x == 0:
+    #         return True
+    #     n = floor(log10(x)+1)
+    #     p = 0
+    #     print(n)
+    #     for i in range (n//2):
+    #         if (x//(10**p))%10 != (x//(10**(n-1-p)))%10:
+    #             return False
+    #         p += 1
+    #     return True
     def isPalindrome(self, x: int) -> bool:
         x_str = str(x)
         for i in range(len(x_str) // 2):
@@ -12,4 +27,6 @@ class Solution(object):
 
 
 # print(Solution().isPalindrome(121))
+# print(Solution().isPalindrome(1000021))
+# print(Solution().isPalindrome(12345654322))
 # print(Solution().isPalindrome(-121))
