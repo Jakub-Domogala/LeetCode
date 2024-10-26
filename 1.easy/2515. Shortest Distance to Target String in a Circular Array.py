dd79686d9789117b4a0951b3d9b2048c261ec890
Jakub-Domogala <kuba.domogala@gmail.com>
2024-10-26 14:39:29 +0200

    2515

---
+++ 1.easy/2515. Shortest Distance to Target String in a Circular Array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def closetTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        cnt = 0
+        n = len(words)
+        while words[(startIndex+cnt)%n] != target and words[startIndex-cnt] != target:
+            cnt += 1
+            if cnt == n//2 + 1:
+                return -1
+        return cnt
+
+words = ["hello","i","am","leetcode","hello"]
+target = "hello"
+startIndex = 1
+result = Solution().closetTarget(words, target, startIndex)
+print(result)
