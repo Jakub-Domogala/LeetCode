edc315640bd4ac89ed20cecbe4e8f5dcebdf3e37
Jakub-Domogala <kuba.domogala@gmail.com>
2024-11-03 17:44:59 +0100

    0171

--- 1.easy/0171. Excel Sheet Column Number.py
+++ 1.easy/0171. Excel Sheet Column Number.py
@@ -2,9 +2,10 @@ class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
         while len(columnTitle) > 0:
+            result *= 26
             result += ord(columnTitle[0]) - ord("A") + 1
             columnTitle = columnTitle[1:]
-            result *= 26
+        return result
 
 print(Solution().titleToNumber("A"))
 print(Solution().titleToNumber("AB"))
