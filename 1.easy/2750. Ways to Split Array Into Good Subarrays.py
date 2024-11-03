eac98e564e333cfb1d12429108b53ae736e30ba3
Jakub-Domogala <kuba.domogala@gmail.com>
2024-11-04 00:13:03 +0100

    2750

---
+++ 1.easy/2750. Ways to Split Array Into Good Subarrays.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
+        options = [1]
+        last = None
+        cnt = 0
+        for i in nums:
+            if i == 0:
+                options[-1] += 1
+            else:
+                cnt += 1
+                options.append(1)
+        options = options[1:cnt]
+        def multi(A, i=0):
+            if i == len(A):
+                return 1
+            return (A[i] * multi(A,i+1))%(10**9+7)
+        if len(options) == 0:
+            return 1 if cnt > 0 else 0
+        return multi(options)
+
+# nums = [0,1,0,0,1]
+# result = Solution().numberOfGoodSubarraySplits(nums)
+# print(result, 3)
+# nums = [1,0,0,1]
+# result = Solution().numberOfGoodSubarraySplits(nums)
+# print(result, 3)
+# nums = [1,0,0,1,0,0]
+# result = Solution().numberOfGoodSubarraySplits(nums)
+# print(result, 3)
+# nums = [0,1,0]
+# result = Solution().numberOfGoodSubarraySplits(nums)
+# print(result, 0)
