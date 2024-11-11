96cbb769a0512e4baa4c713851fd9106e5d25e2f
Jakub-Domogala <kuba.domogala@gmail.com>
2024-11-11 22:45:42 +0100

    2302

---
+++ 3.hard/2302. Count Subarrays With Score Less Than K.py
@@ -0,0 +1,28 @@
+# Time Complexity:   O(n)
+# Memory Complexity: O(1)
+
+
+from typing import List
+
+class Solution:
+    def countSubarrays(self, nums: List[int], k: int) -> int:
+        l_id, r_id = 0, 0
+        c_score = 0
+        cnt = 0
+        while r_id < len(nums):
+            if r_id == len(nums):
+                done = True
+            c_score += nums[r_id]
+            r_id += 1
+            while c_score * (r_id - l_id) >= k:
+                c_score -= nums[l_id]
+                l_id += 1
+            cnt += (r_id - l_id)
+        return cnt
+
+# nums = [2,1,4,3,5]
+# k = 10
+# print(Solution().countSubarrays(nums, k), 6)
+# nums = [1,1,1]
+# k = 5
+# print(Solution().countSubarrays(nums, k), 5)
