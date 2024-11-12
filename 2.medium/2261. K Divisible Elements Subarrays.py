4ebf6682a215839a1fe5a3152589a4ed8fdb1baf
Jakub-Domogala <kuba.domogala@gmail.com>
2024-11-12 03:19:50 +0100

    2261

--- 2.medium/2261. K Divisible Elements Subarrays.py
+++ 2.medium/2261. K Divisible Elements Subarrays.py
@@ -1,24 +1,32 @@
+# Time Complexity:   O(n^2)
+# Memory Complexity: O(n^2)
+#
+# making a set of hashes of arrays instead of arrays could be a quicker solution (also O(n^2))
+
+
 from typing import List
 
 class Solution:
     def countDistinct(self, nums: List[int], k: int, p: int) -> int:
-        options = [1]
-        cnt = 0
-        for i in nums:
-            if i%p == 0:
-                options[-1] += 1
-            else:
-                cnt += 1
-                options.append(1)
-        print(options)
-        l, r = 0, k
+        l,r = 0,0
+        n = len(nums)
+        div_count = 0
+        arrays = set()
         result = 0
-        while r < len(options):
-            result += options[l] * options[r]
-            l += 1
+        while r < n:
+            if nums[r] % p == 0:
+                div_count += 1
             r += 1
+            while div_count > k:
+                if nums[l] % p == 0:
+                    div_count -= 1
+                l += 1
+            result += r - l
+            for i in range(l,r):
+                arrays.add(tuple(x for x in nums[i:r]))
+        return len(list(arrays))
 
-nums = [2,3,3,2,2]
-k = 2
-p = 2
-print(Solution().countDistinct(nums, k, p))
+# nums = [2,3,3,2,2]
+# k = 2
+# p = 2
+# print(Solution().countDistinct(nums, k, p))
